18033eddd2e28918ea4fd6fb2b11b2cba7995d75
Jeevan Bhargav <g.jeevanbhargav@gmail.com>
2023-10-19 14:05:17 -0500

    Resolving Promises

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
+// const { welcome, goodbye, tell } = require("../utils/fortune-teller");
+
+// const question = "Will the weather be nice today"
+
+// const promise = tell(question);
+
+// promise
+
+// .then((fortune) => {
+//     console.log(question)
+//     console.log(fortune)
+
+// })
+// .catch(console.error);
+
+
+//This Can be Modified like this:
+//a.
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
-const promise = welcome();
+const question = ""
 
-console.log(promise);
+tell(question)
+.then((fortune) => {
+    console.log(question)
+    console.log(fortune)
+
+})
+.catch(console.error); //it isnot necessary to put catch statements at he end because if there's an error catch will be called first.
+
+//b.
+welcome()
+  .then(console.log) // Logs the result of welcome()
+  .then(() => goodbye().then(console.log)) // .then inside of another .then
+  .catch(console.error); // Logs error from welcome() or goodbye()
